81df20d2b625a8b1b1185cf69aae9c13122e75db
aliciaVillon <38113963+aliciaVillon@users.noreply.github.com>
2025-02-10 04:00:33 -0500

    feat: se completa crud para customers

---
+++ src/test/java/proyecto1/msconfigserver/MsconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto1.msconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
